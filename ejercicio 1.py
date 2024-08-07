cdf75153c6b9f30b3e00fafe979ef4f9200b7209
Mari Luz Jimenez <mariluzj799@gmail.com>
2024-08-07 12:22:43 -0500

    ejercicio 1

---
+++ ejercicio 1.py
@@ -0,0 +1,7 @@
+# definir una funcion max()que tome como argumento dos numeros y devuelva el mayor de ellos.
+def Max(num1, num2):
+    return max(num1, num2)
+
+# Ejemplo de uso
+numero_mayor = Max(5, 10)
+print("El número mayor es:", numero_mayor)
