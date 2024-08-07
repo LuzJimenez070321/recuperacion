eba6deb4235c52eeab1a7858ceca859215cbb0a2
Mari Luz Jimenez <mariluzj799@gmail.com>
2024-08-07 12:31:11 -0500

    ejercicio 2

---
+++ ejercici 2.py
@@ -0,0 +1,7 @@
+# Definir una funcion max_de_tres , tome tres numeros como argumentos y devuelva el mayor de ellos 
+def max_de_tres(num1, num2, num3):
+    return max(num1, num2, num3)
+
+
+numero_mayor = max_de_tres(15, 8, 20)
+print("El número mayor es:", numero_mayor)
